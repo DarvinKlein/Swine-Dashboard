8313acd665e4ed1e1772bcf8ed6c9c1412290ccb
Darvin Klein Momo <50259631+DarvinKlein@users.noreply.github.com>
2026-07-07 17:39:38 +0800

    Update app.php

--- swine-dashboard-api/bootstrap/app.php
+++ swine-dashboard-api/bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
+    ->withMiddleware(function (Middleware $middleware) {
+    $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
