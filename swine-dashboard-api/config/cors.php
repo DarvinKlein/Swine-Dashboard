758bbd45aafe5ccf93b198243630f7e1d79a05ba
Darvin Klein Momo <50259631+DarvinKlein@users.noreply.github.com>
2026-07-07 17:36:10 +0800

    Update cors.php
    update cors.php

--- swine-dashboard-api/config/cors.php
+++ swine-dashboard-api/config/cors.php
@@ -6,6 +6,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['http://localhost:5173'],
+    
+    'allowed_origins_patterns' => ['#^https://swine-dashboard-.*-darvinkleins-projects\.vercel\.app$#'],
 
     'allowed_origins_patterns' => [],
 
